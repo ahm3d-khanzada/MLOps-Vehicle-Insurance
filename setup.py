456becde8ea2667ac8c1741014973acd48b5b8cd
ahmedkhanzada299-wq <ahmedkhanzada299@gmail.com>
2026-05-17 15:44:49 +0500

    Project Structure ready and Venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ahmed Khanzada",
+    author_email="ahm3d.workspace@gmail.com",
+    packages=find_packages()
+)
